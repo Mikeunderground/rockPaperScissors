733cb2e6fbcecbe0f90aebbc27a69853ca2d714b
Mikeunderground <mikw1534@gmail.com>
2021-03-03 20:57:22 -0500

    Added game(), playRound() funcitons and for loop to execute the game 5 times

--- script.js
+++ script.js
@@ -1,51 +1,95 @@
-function computerPlay(){
-    randomNum = Math.floor(Math.random() * 10 - 1);
-    console.log(randomNum)
 
-    switch(randomNum){
-        case 0:
-            computerPlay();
-            break;
-        case 1:
-            console.log('Rock');
-            computerSelection = "Rock";
-            break;
-        case 2:
-            console.log('Rock');
-            computerSelection = "Rock";
-            break;
-        case 3:
-            console.log('Rock');
-            computerSelection = "Rock";
-            break;
-        case 4:
-            console.log('Paper');
-            computerSelection = "Paper";
-            break;
-        case 5:
-            console.log('Paper');
-            computerSelection = "Paper";
-            break;
-        case 6:
-            console.log('Paper');
-            computerSelection = "Paper";
-            break;
-        case 7:
-            console.log('Scissors');
-            computerSelection = "Scissors";
-            break;
-        case 8:
-            console.log('Scissors');
-            computerSelection = "Scissors";
-            break;
-        case 9:
-            console.log('Scissors');
-            computerSelection = "Scissors";
-            break;
-    }
+const playerSelection = "Rock";
+
+for(let i = 0; i < 5; i++){
+    game(); 
 }
 
-function (playerSelection, computerSelection)
+function game(){
+    
 
+        function computerPlay(){
+            randomNum = Math.floor(Math.random() * 10 - 1);
+                if(randomNum === -1){
+                    return randomNum + 2;
+                }
+        
+        
+            console.log(randomNum)
+        
+            var computerSelection
+        
+            switch(randomNum){
+                case 0:
+                    console.log('Rock');
+                    computerSelection = "Rock";
+                    break;
+                case 1:
+                    console.log('Rock');
+                    computerSelection = "Rock";
+                    break;
+                case 2:
+                    console.log('Rock');
+                    computerSelection = "Rock";
+                    break;
+                case 3:
+                    console.log('Rock');
+                    computerSelection = "Rock";
+                    break;
+                case 4:
+                    console.log('Paper');
+                    computerSelection = "Paper";
+                    break;
+                case 5:
+                    console.log('Paper');
+                    computerSelection = "Paper";
+                    break;
+                case 6:
+                    console.log('Paper');
+                    computerSelection = "Paper";
+                    break;
+                case 7:
+                    console.log('Scissors');
+                    computerSelection = "Scissors";
+                    break;
+                case 8:
+                    console.log('Scissors');
+                    computerSelection = "Scissors";
+                    break;
+                case 9:
+                    console.log('Scissors');
+                    computerSelection = "Scissors";
+                    break;
+            }
+            return computerSelection;
+            
+        }
+        
+        function playRound(playerSelection, computerSelection){
+            
+            if(playerSelection === computerSelection){
+                return "IT'S A TIE!!!"
+            } else if (playerSelection == 'Rock' & computerSelection == 'Paper'){
+                return "Paper beats rock! Computer wins!";
+            } else if (playerSelection == 'Paper' & computerSelection == 'Scissors'){
+                return "Scissors beats paper! Computer wins!"
+            } else if (playerSelection == 'Scissors' & computerSelection == 'Rock'){
+                return "Rock beats scissors! Computer wins!"
+            } else if (playerSelection == 'Rock' & computerSelection == 'Scissors'){
+                return "Rock beats scissors! Player wins!"
+            } else if (playerSelection == 'Paper' & computerSelection == 'Rock'){
+                return "Paper beats rock! Player wins!"
+            } else if (playerSelection == 'Scissors' & computerSelection == 'Paper'){
+                return "Scissors beats paper! player wins!"
+            } else {
+                console.log(computerSelection);
+                return "Wtf just happened?"
+                
+            }
+        }
+    
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
 
-computerPlay();
+};
+
